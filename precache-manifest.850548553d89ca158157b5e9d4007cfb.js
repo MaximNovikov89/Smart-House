f0f16a37d09cd6c757b0a00a60e5774832b566f5
MaximNovikov89 <mthelast@gmail.com>
2020-06-21 15:05:33 +0300

    Updates

---
+++ precache-manifest.850548553d89ca158157b5e9d4007cfb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6efdf6618b3032f5f8d98fc72787ded",
+    "url": "/Smart-House/index.html"
+  },
+  {
+    "revision": "64e05ae4427b33732aa9",
+    "url": "/Smart-House/static/css/main.f6e8e279.chunk.css"
+  },
+  {
+    "revision": "6d50c88a334492ab22ca",
+    "url": "/Smart-House/static/js/2.37a826b8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Smart-House/static/js/2.37a826b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64e05ae4427b33732aa9",
+    "url": "/Smart-House/static/js/main.906965d7.chunk.js"
+  },
+  {
+    "revision": "8952b281f3203b81f602",
+    "url": "/Smart-House/static/js/runtime-main.861f2f76.js"
+  },
+  {
+    "revision": "b4361b79aee98767cfc63e1dfef5535f",
+    "url": "/Smart-House/static/media/Bathroom.b4361b79.jpg"
+  },
+  {
+    "revision": "409ae21bbe34947b565182b445255850",
+    "url": "/Smart-House/static/media/Bedroom.409ae21b.jpg"
+  },
+  {
+    "revision": "0b571be827771f4fe72b2feb4641fe2c",
+    "url": "/Smart-House/static/media/Kitchen.0b571be8.jpg"
+  },
+  {
+    "revision": "63c5741d934e6075b2dd6e4a1bce9164",
+    "url": "/Smart-House/static/media/img.63c5741d.jpg"
+  }
+]);
